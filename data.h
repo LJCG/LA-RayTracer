0954dc6768f847dd5f253d2b6012bd2b89a71a27
Lucy Chaves <lucy.chaves.gamboa@gmail.com>
2016-11-02 19:12:40 -0600

    Error en archivos

--- data.h
+++
@@ -1,8 +0,0 @@
-#ifndef DATA_H
-#define DATA_H
-
-#define MAX_CHARS 50
-
-void loadInfo(void);
-void globalConfig(void);
-#endif
--- lights.h
+++
@@ -1,8 +0,0 @@
-#ifndef LIGHTS_H_   
-#define LIGHTS_H_
-
-LIGHT createLight(POINT location, long double intensity, long double c1, long double c2, long double c3);
-long double getFatt(LIGHT light, VECTOR L);
-long double getIntensity(double pointProd, OBJECT object, long double fatt, long double lightIntensity);
-
-#endif
